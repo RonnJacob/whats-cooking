259430a2ccb30036d69dc34b445da97fcff2edc6
Ronn <jacob.r@husky.neu.edu>
2019-04-22 01:30:22 -0400

    Testing deployment

--- src/services/UserServices.js
+++ src/services/UserServices.js
@@ -1,6 +1,7 @@
 import {getFromStorage} from "../utils/storage";
 
-const FETCH_URL = 'http://localhost:4200/api/';
+// const FETCH_URL = 'http://localhost:4200/api/';
+const FETCH_URL = 'https://cs5610-backend.herokuapp.com/api/';
 export default class UserServices{
     checkUserNameValidity = (uname) => {
         return fetch(FETCH_URL + 'username/'+uname)
