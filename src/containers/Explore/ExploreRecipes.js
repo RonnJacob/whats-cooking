e5c61d294d81d9751050dbf1fc69d50ef66e09ed
amrita-nair <amritanair@ccs.neu.edu>
2019-04-22 12:59:09 -0400

    Added Filter and sort recipes

--- src/containers/Explore/ExploreRecipes.js
+++ src/containers/Explore/ExploreRecipes.js
@@ -2,10 +2,7 @@ import React from 'react'
 
 import MealDBServices from "../../services/MealDBServices";
 import RecipeCard from "../LandingPage/RecipeCard";
-import {LandingPageHeader} from "../LandingPage/LandingPageHeader";
-import HomePageNav from "../../components/HomePageNav";
 import GuestNav from "../LandingPage/GuestNav";
-import {PopularRecipes} from "../LandingPage/PopularRecipes";
 import '../../assets/landingpage/css/sidebar.css'
 import FilterRecipes from "./FilterRecipes";
 
@@ -15,97 +12,74 @@ class ExploreRecipes extends React.Component {
         this.mealDBServices = new MealDBServices();
         this.state = {
             recipes: [],
-            searchRecipe:'',
-            popularRecipes:[],
-            filterCategory:[]
+            searchRecipe: '',
+            popularRecipes: [],
+            filterCategory: []
 
         };
-        this.searchRecipe= this.searchRecipe.bind(this);
+        this.searchRecipe = this.searchRecipe.bind(this);
 
     }
 
     componentWillMount() {
         document.title = "Explore Recipes";
         this.mealDBServices.findAllRecipes()
-            .then(recipes =>{
+            .then(recipes => {
                 this.setState
-                ({recipes: recipes})
+                ({recipes: recipes.meals})
             });
     }
 
     componentDidMount() {
-       this.mealDBServices.findAllCategories()
-           .then(filterCategory =>{
-               this.setState
-               ({filterCategory: filterCategory.meals})
-           });
+        this.mealDBServices.findAllCategories()
+            .then(filterCategory => {
+                this.setState
+                ({filterCategory: filterCategory.meals})
+            });
     }
 
     findAllCuisines = () => {
         this.mealDBServices.findAllCuisines()
-            .then(filterCategory =>{
+            .then(filterCategory => {
                 this.setState
                 ({filterCategory: filterCategory.meals})
-            });}
-
-    myFunction(id) {
-        var x = document.getElementById(id);
-        if(x) {
-            if (x.className.indexOf("w3-show") === -1) {
-                x.className += " w3-show";
-            } else {
-                x.className = x.className.replace(" w3-show", "");
-            }
-        }
-    }
-
-    filterFunction() {
-        var input, filter, ul, li, a, i;
-        input = document.getElementById("myInput");
-        filter = input.value.toUpperCase();
-        var div = document.getElementById("myDropdown");
-        a = div.getElementsByTagName("a");
-        for (i = 0; i < a.length; i++) {
-            var txtValue = a[i].textContent || a[i].innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                a[i].style.display = "";
-            } else {
-                a[i].style.display = "none";
-            }
-        }
+            });
     }
 
     findAllCategories = () => {
         this.mealDBServices.findAllCategories()
-            .then(filterCategory =>{
+            .then(filterCategory => {
                 this.setState
                 ({filterCategory: filterCategory.meals})
-            });}
+            });
+    }
 
     findRecipesByCategory = (category) => {
         this.mealDBServices.findRecipesByCategory(category)
-            .then(recipes =>{
+            .then(recipes => {
                 this.setState
-                ({recipes: recipes})
-            });}
+                ({recipes: recipes.meals})
+            });
+    }
 
-    findRecipesByCuisine= (cuisine) => {
+    findRecipesByCuisine = (cuisine) => {
         this.mealDBServices.findRecipesByCuisines(cuisine)
-            .then(recipes =>{
+            .then(recipes => {
                 this.setState
-                ({recipes: recipes})
-            });}
+                ({recipes: recipes.meals})
+            });
+    }
 
     searchRecipe = (recipe) => {
-        this.mealDBServices.findRecipeByName(recipe).then(recipes =>{
+        this.mealDBServices.findRecipeByName(recipe).then(recipes => {
 
             this.setState
-            ({recipes: recipes})
-            if(recipes.meals==null){
-                document.getElementById("food-area").innerHTML=
+            ({recipes: recipes.meals})
+            if (recipes.meals == null) {
+                document.getElementById("food-area").innerHTML =
                     " <div class=\"no-results\">" +
                     "<h3>We searched all over but didn't " +
-                    "find a recipe for '"+`${recipe}`+"'</h3>" +
+                    "find a recipe for '" + `${recipe}` + "'</h3>" +
 
                     "<div class=\"no-results-suggestion\">" +
                     "<img src=\"https://x.yummlystatic.com/s/e3ccfc5a7/img/check_spelling.svg\">" +
@@ -115,19 +89,62 @@ class ExploreRecipes extends React.Component {
                     "<img src=\"https://x.yummlystatic.com/s/e3ccfc5a7/img/simplify_search.svg\">" +
                     "<span>Simplify Search</span></div></div>" +
                     "" +
-                    " </PopularRecipes popularRecipes={"+this.state.popularRecipes+"}>"
+                    " </PopularRecipes popularRecipes={" + this.state.popularRecipes + "}>"
 
 
             }
         });
     }
+
+
+    compareAsc(a, b) {
+        // Use toUpperCase() to ignore character casing
+        const recipeA = a.strMeal.toUpperCase();
+        const recipeB = b.strMeal.toUpperCase();
+
+        let comparison = 0;
+        if (recipeA > recipeB) {
+            comparison = 1;
+        } else if (recipeA < recipeB) {
+            comparison = -1;
+        }
+        return comparison;
+    }
+    compareDesc(a, b) {
+        // Use toUpperCase() to ignore character casing
+        const recipeA = a.strMeal.toUpperCase();
+        const recipeB = b.strMeal.toUpperCase();
+
+        let comparison = 0;
+        if (recipeA > recipeB) {
+            comparison = 1;
+        } else if (recipeA < recipeB) {
+            comparison = -1;
+        }
+        return comparison*-1;
+    }
+
+    sortAscend = () => {
+        var sortedRecipes=this.state.recipes.sort(this.compareAsc);
+        this.setState
+            (
+                {recipes: sortedRecipes}
+                )
+        alert(this.state.recipes.length)
+    };
+
+    sortDescend = () => {
+        var sortedRecipes=this.state.recipes.sort(this.compareDesc);
+        this.setState
+        ({recipes: sortedRecipes})
+    };
+
     searchChanged = (event) => {
         this.setState(
             {
                 searchRecipe: event.target.value
             });
-
-    }
+    };
 
     render() {
         return (
@@ -135,88 +152,104 @@ class ExploreRecipes extends React.Component {
             <div>
                 {/*<LandingPageHeader/>*/}
                 <div id="header">
-                <GuestNav/>
+                    <GuestNav/>
                 </div>
                 <div>
-                <section className="table-area section-padding" >
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-lg-12">
-                                <div className="section-top2 text-center">
-                                    <h3>Find <span>a</span> recipe</h3>
-                                    <p><i>Time to get into a yummilicious world.</i></p>
+                    <section className="table-area section-padding">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-lg-12">
+                                    <div className="section-top2 text-center">
+                                        <h3>Find <span>a</span> recipe</h3>
+                                        <p><i>Time to get into a yummilicious world.</i></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-lg-8 offset-lg-2">
+                                    <form action="#">
+
+
+                                        <div className="form-group has-search">
+                                            <span className="fa fa-search form-control-feedback"></span>
+                                            <input type="text" className="form-control"
+                                                   placeholder="search a million recipes & more"
+                                                   onChange={this.searchChanged}/>
+                                        </div>
+
+                                        <div className="table-btn text-center">
+                                            <a href="#" className="template-btn template-btn2 mt-4"
+                                               onClick={() => this.searchRecipe(this.state.searchRecipe)}>Go</a>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
                         </div>
-                        <div className="row">
-                            <div className="col-lg-8 offset-lg-2">
-                                <form action="#">
-
-
-                                    <div className="form-group has-search">
-                                        <span className="fa fa-search form-control-feedback"></span>
-                                        <input type="text" className="form-control" placeholder="search a million recipes & more" onChange={this.searchChanged}/>
-                                    </div>
-
-                                    <div className="table-btn text-center">
-                                        <a href="#" className="template-btn template-btn2 mt-4"
-                                           onClick={() => this.searchRecipe(this.state.searchRecipe)}>Go</a>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+                    </section>
                 </div>
 
                 <section className="header">
-<div className={"row"}>
-    <div className="split left">
-                    <div className={"side-menu"}>
+                    <div className={"row"}>
+                        <div className="split left">
+                            <div className={"side-menu"}>
 
-                        <div className="">
+                                <div className="">
 
-                            <div className="sidebar-categories">
-                                <div className="head">Filter up your TasteBuds</div>
-                                <ul className="main-categories">
-
-
-
-                                    {this.state.filterCategory.map(category =>
-                                        <FilterRecipes category={category.strCategory}
-                                                       findRecipesByCategory={this.findRecipesByCategory}
-                                                       findRecipesByCuisine={this.findRecipesByCuisine}/>
-                                    )}
+                                    <div className="sidebar-categories">
+                                        <div className="head">Filter up your TasteBuds</div>
+                                        <ul className="main-categories">
 
 
+                                            {this.state.filterCategory.map(category =>
+                                                <FilterRecipes category={category.strCategory}
+                                                               findRecipesByCategory={this.findRecipesByCategory}
+                                                               findRecipesByCuisine={this.findRecipesByCuisine}/>
+                                            )}
 
 
-                                </ul>
-                            </div></div>
-                    </div></div>
-    <div className="split right">
-                    <div className="food-area " id={"food-area"}>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="split right">
+                            <div className="filter-bar d-flex flex-wrap align-items-center">
+                                <div className="sorting">
 
-                        <div className="container">
-                            <div className="row">
+                                </div>
+                                <div className="sorting mr-auto">
+
+                                </div>
+                                <div className="pagination">
+                                    <a href="#" className="prev-arrow head" onClick={this.sortAscend}><i className="fa fa-sort-alpha-asc"
+                                                                          aria-hidden="true"></i></a>
+                                    <a href="#" className="next-arrow head" onClick={this.sortDescend}><i className="fa fa-sort-alpha-desc"
+                                                                          aria-hidden="true"></i></a>
+
+                                    <a href="#">reset</a>
+
+                                </div>
+                            </div>
+                            <div className="food-area " id={"food-area"}>
+
+                                <div className="container">
+                                    <div className="row">
 
 
-
-
-
-                                {
-                                    this.state.recipes.meals &&
-                                    this.state.recipes.meals .map(recipe =>
-                                        <RecipeCard popularRecipe={recipe}/>
-                                    )
-                                }
+                                        {
+                                            this.state.recipes &&
+                                            this.state.recipes.map(recipe =>
+                                                <RecipeCard popularRecipe={recipe}/>
+                                            )
+                                        }
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-    </div></div>
                 </section>
 
-</div>
+            </div>
         )
     }
 }
